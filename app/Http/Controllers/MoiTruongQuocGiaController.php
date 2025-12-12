b3f63e694ffb8622f04428afdefc2b6b6131f59e
trunglnk <81243093+trunglnk@users.noreply.github.com>
2025-12-12 10:24:49 +0700

    fix ssl

--- app/Http/Controllers/MoiTruongQuocGiaController.php
+++ app/Http/Controllers/MoiTruongQuocGiaController.php
@@ -108,8 +108,8 @@ class MoiTruongQuocGiaController extends Controller
             $from = $from->toIso8601ZuluString();
             $url = "$url&from=$from";
         }
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
         curl_setopt_array($curl, array(
             CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
